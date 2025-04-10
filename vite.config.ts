12b0279b8c595fb801c7b4311021b0d86cc7421b
Igor Ilić <igor.ilic@heidelbergcement.com>
2025-04-11 00:19:44 +0200

    chore: vite changed

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
 import { vitePlugin as remix } from "@remix-run/dev";
+import { installGlobals } from "@remix-run/node";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 import { vercelPreset } from "@vercel/remix/vite";
@@ -9,16 +10,18 @@ declare module "@remix-run/node" {
   }
 }
 
+installGlobals();
+
 export default defineConfig({
   plugins: [
     remix({
       presets: [vercelPreset()],
       future: {
-        v3_fetcherPersist: true,
-        v3_relativeSplatPath: true,
-        v3_throwAbortReason: true,
-        v3_singleFetch: true,
-        v3_lazyRouteDiscovery: true,
+        // v3_fetcherPersist: true,
+        // v3_relativeSplatPath: true,
+        // v3_throwAbortReason: true,
+        // v3_singleFetch: true,
+        // v3_lazyRouteDiscovery: true,
       },
     }),
     tsconfigPaths(),
